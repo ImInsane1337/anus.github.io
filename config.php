fbe79cb83716349445250d608a4f70b8b154d0ee
ImInsane1337 <insanexware@gmail.com>
2024-07-29 21:15:32 +0400

    123

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "your_database_name";
+$servername = "89.23.117.121";
+$username = "ImInsane";
+$password = "Fade3322";
+$dbname = "clicker";
 
 // Создание подключения
 $conn = new mysqli($servername, $username, $password, $dbname);
--- get_coins.php
+++ get_coins.php
@@ -16,3 +16,4 @@ if ($username) {
 
 $conn->close();
 ?>
+    
